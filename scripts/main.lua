a1ae369ad4129ad4e08739f17cab8e45b6797fec
Diyagi <github@diyagi.dev>
2025-12-04 13:02:25 -0300

    Improves registerhook

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,19 +1,26 @@
-ModName, ModVersion = "Stack Tweaks", "1.0.2"
+ModName, ModVersion = "Stack Tweaks", "1.0.3"
 local stackTweaks = require("stackTweaks")
 
-ExecuteInGameThread(function()
-    LoadAsset("/Game/SurvivalGameKitV2/Blueprints/Characters/BP_SGKController.BP_SGKController_C")
-    RegisterHook("/Game/SurvivalGameKitV2/Blueprints/Characters/BP_SGKController.BP_SGKController_C:ReceiveBeginPlay", function(self)
-        local masterItemList = StaticFindObject("/Game/SurvivalGameKitV2/Blueprints/Items/MasterLists/MasterItemList.MasterItemList")
-        
-        masterItemList:ForEachRow(function(rowName, rowData)
-            local stackConfig = stackTweaks[rowName]
-            if not stackConfig then return end
+LoopAsync(2000, function()
+    local sgkController = StaticFindObject("/Game/SurvivalGameKitV2/Blueprints/Characters/BP_SGKController.BP_SGKController_C")
+    if not sgkController:IsValid() then return false end
+    
+    
+    ExecuteWithDelay(2000, function()
+        RegisterHook("/Game/SurvivalGameKitV2/Blueprints/Characters/BP_SGKController.BP_SGKController_C:ReceiveBeginPlay", function(self)
+            local masterItemList = StaticFindObject("/Game/SurvivalGameKitV2/Blueprints/Items/MasterLists/MasterItemList.MasterItemList")
             
-            rowData.AllowStacking_48_02CFD0D74B05709E8E432E824709C714 = stackConfig.allowStack
-            rowData.MaxStack_18_C57243124FFDE0AB28797AA6F2C1E5F5 = stackConfig.stackSize
+            masterItemList:ForEachRow(function(rowName, rowData)
+                local stackConfig = stackTweaks[rowName]
+                if not stackConfig then return end
+                
+                rowData.AllowStacking_48_02CFD0D74B05709E8E432E824709C714 = stackConfig.allowStack
+                rowData.MaxStack_18_C57243124FFDE0AB28797AA6F2C1E5F5 = stackConfig.stackSize
+            end)
+            
+            print("Stack Tweaks Applied!")
         end)
-        
-        print("Stack Tweaks Applied!")
     end)
+    
+    return true
 end)
