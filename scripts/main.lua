ed132b889df4e5053dd0f98da7852eb491b849cf
Diyagi <github@diyagi.dev>
2025-12-04 06:50:42 -0300

    Add delay to config loading

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,17 +1,21 @@
-ModName, ModVersion = "Stack Tweaks", "1.0.0"
+ModName, ModVersion = "Stack Tweaks", "1.0.1"
 local stackTweaks = require("stackTweaks")
 
-ExecuteInGameThread(function()
-    LoadAsset("/Game/SurvivalGameKitV2/Blueprints/Items/MasterLists/MasterItemList.MasterItemList")
-    local masterItemList = StaticFindObject("/Game/SurvivalGameKitV2/Blueprints/Items/MasterLists/MasterItemList.MasterItemList")
+local preId, posId = nil, nil
 
-    masterItemList:ForEachRow(function(rowName, rowData)
-        local stackConfig = stackTweaks[rowName]
-        if not stackConfig then return end
-
-        rowData.AllowStacking_48_02CFD0D74B05709E8E432E824709C714 = stackConfig.allowStack
-        rowData.MaxStack_18_C57243124FFDE0AB28797AA6F2C1E5F5 = stackConfig.stackSize
+-- TODO: Find a function to hook into for this, relying on delay is badge
+ExecuteWithDelay(3000, function()
+    ExecuteInGameThread(function()
+        local masterItemList = StaticFindObject("/Game/SurvivalGameKitV2/Blueprints/Items/MasterLists/MasterItemList.MasterItemList")
+        
+        masterItemList:ForEachRow(function(rowName, rowData)
+            local stackConfig = stackTweaks[rowName]
+            if not stackConfig then return end
+            
+            rowData.AllowStacking_48_02CFD0D74B05709E8E432E824709C714 = stackConfig.allowStack
+            rowData.MaxStack_18_C57243124FFDE0AB28797AA6F2C1E5F5 = stackConfig.stackSize
+        end)
+        
+        print("Stack Tweaks Applied!")
     end)
-
-    print("Stack Tweaks Applied!")
 end)
